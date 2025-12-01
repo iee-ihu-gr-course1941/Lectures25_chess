c9af98b053a1d8158c4e472cf4d7d39e8d1f97fe
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2025-12-01 18:08:17 +0200

    1/12

---
+++ lecture2/chess2_01.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once "lib/dbconnect.php";
+require_once "lib/board.php";
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
+// Σε περίπτωση που τρέχουμε php –S 
+$input = json_decode(file_get_contents('php://input'),true);
+
+switch ($r=array_shift($request)) {
+  case 'board' : 
+		switch ($b=array_shift($request)) {
+			case '':
+			case null: 
+			  handle_board($method);
+			  break;
+			case 'piece': 
+			  handle_piece($method, $request[0], $request[1], $input);
+				break;
+			case 'player': 
+			  handle_player($method, $request[0], $input);
+				break;
+			default: 
+			  header("HTTP/1.1 404 Not Found");
+				break;
+	}
+	break;
+  // New code...	
+	case 'status': 
+		if(sizeof($request)==0) {
+			handle_status($method);
+		} else {
+			header("HTTP/1.1 404 Not Found");
+		}
+		break;
+	case 'players': 
+	  handle_player($method, $request, $input);
+		break;
+  default: 	
+	  header("HTTP/1.1 404 Not Found");
+    print "<h1>not FOUND</h1>";
+	  exit;
+  // End New code...	
+}
+
+function handle_board($method) {
+    if($method=='GET') {
+            show_board();
+    } else if ($method=='POST') {
+           reset_board();
+    } else {
+        header('HTTP/1.1 405 Method Not Allowed');
+    }
+}
+
+// New Code...
+function handle_piece($method, $x, $y, $input) {
+    print("x=$x, y=$y");
+    print_r($input);
+}
+
+function handle_player($method, $p, $input) {
+    ;
+}
+
+function handle_status($method) {
+    if($method=='GET') {
+        show_status();
+    } else {
+        header('HTTP/1.1 405 Method Not Allowed');
+    }
+}
+// End new code...
+
+?>
